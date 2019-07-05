7a069692856ff70fc00433697af5ecd710fef043
Li-Yangchen <lyc19920828@126.com>
2019-07-05 18:09:38 +0800

    97页

--- mydemo/myweb/views.py
+++ mydemo/myweb/views.py
@@ -73,6 +73,32 @@ def search_realname(request):
 #签到页面
 @login_required
 def sign_index(request,eid):
+    username = request.session.get('user', '')
     event = get_object_or_404(Event,id=eid)
     return render(request,'sign_index.html',{'event':event})
+
+@login_required
+def sign_index_action(request,eid):
+    username = request.session.get('user', '')
+    event = get_object_or_404(Event,id=eid)
+    phone = request.POST.get('phone','')
+    print(phone)
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'phone error !'})
+    result = Guest.objects.filter(phone=phone,event_id=eid)
+    if not result:
+        return render(request,'sign_index.html',{'event':event,'hint':'event id or phone error !'})
+    result = Guest.objects.get(phone=phone,event_id=eid)
+    if result.sign:
+        return render(request,'sign_index.html',{'event':event,'hint':'user has sign in !'})
+    else:
+        Guest.objects.filter(phone=phone,event_id=eid).update(sign='1')
+        return render(request,'sign_index.html',{'event':event,'hint':'sign in success !','guest':result})
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
 
