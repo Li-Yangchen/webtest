5f1125361723b45e69e26c32532a6c9ce3722d81
Li-Yangchen <lyc19920828@126.com>
2019-07-08 17:50:04 +0800

    100页,单元测试

--- mydemo/myweb/tests.py
+++ mydemo/myweb/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from myweb.models import Event,Guest
 
 # Create your tests here.
+class ModelTest(TestCase):
+    def setUp(self):
+        Event.objects.create(id=1,name="oneplus 3 event",status=True,limit=2000,address='shenzhen',start_time='2019-07-01 10:00:00')
+        Guest.objects.create(id=1,event_id=1,realname='alen',phone='12345678908')
