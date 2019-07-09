260d3e7d8147cc7cd0ce7b65af51d540e8d3ddd1
Li-Yangchen <lyc19920828@126.com>
2019-07-09 18:03:31 +0800

    104页,单元测试

--- mydemo/myweb/tests.py
+++ mydemo/myweb/tests.py
@@ -7,3 +7,10 @@ class ModelTest(TestCase):
         Event.objects.create(id=1,name="oneplus 3 event",status=True,limit=2000,address='shenzhen',start_time='2019-07-01 10:00:00')
         Guest.objects.create(id=1,event_id=1,realname='alen',phone='12345678908',email='alen@mail.com',sign=False)
     def test_event_models(self):
+        result=Event.objects.get(name="oneplus 3 event")
+        self.assertAlmostEqual(result.address,"shenzhen")
+        self.assertTrue(result.status)
+    def test_guest_models(self):
+        result=Guest.objects.get(phone='12345678908')
+        self.assertEqual(result.realname,"alen")
+        self.assertFalse(result.sign)
