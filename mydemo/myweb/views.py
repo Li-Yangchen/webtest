bc5154e092bf4d213c0685f9b17f48cf394c6965
Li-Yangchen <lyc19920828@126.com>
2021-03-05 16:58:45 +0800

    20210106 修改event_manage.html中title

--- mydemo/myweb/views.py
+++ mydemo/myweb/views.py
@@ -1,48 +1,53 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from myweb.models import Event,Guest
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from myweb.models import Event, Guest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
 
 def login_action(request):
     if request.method == 'POST':
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
-        user = auth.authenticate(username=username,password=password)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             response = HttpResponseRedirect('/event_manage/')
-#            response.set_cookie('user',username,3600)       #添加cookie
-            request.session['user']=username
+            #            response.set_cookie('user',username,3600)       #添加cookie
+            request.session['user'] = username
             return response
         else:
-            return render(request,'index.html',{'error':'username or password error!'})
-#管理
+            return render(request, 'index.html', {'error': 'username or password error!'})
+
+
+# 管理
 @login_required
 def event_manage(request):
-#   username = request.COOKIES.get('user','')
-    username = request.session.get('user','')
+    #   username = request.COOKIES.get('user','')
+    username = request.session.get('user', '')
     event_list = Event.objects.all()
-    return render(request,"event_manage.html",{"user":username,"events":event_list})
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
 
-#发布会名称搜索
+
+# 发布会名称搜索
 @login_required
 def search_name(request):
-    username = request.session.get('user','')
-    search_name = request.GET.get("name","")
+    username = request.session.get('user', '')
+    search_name = request.GET.get("name", "")
     event_list = Event.objects.filter(name__contains=search_name)
-    return render(request,"event_manage.html",{"user":username,"events":event_list})
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
 
-#嘉宾管理
+
+# 嘉宾管理
 @login_required
 def guest_manage(request):
-    username = request.session.get('user','')
+    username = request.session.get('user', '')
     guest_list = Guest.objects.all()
     paginator = Paginator(guest_list, 3)
     page = request.GET.get('page')
@@ -54,11 +59,12 @@ def guest_manage(request):
         contacts = paginator.page(paginator.num_pages)
     return render(request, "guest_manage.html", {"user": username, "guests": contacts})
 
-#姓名搜索
+
+# 姓名搜索
 @login_required
 def search_realname(request):
-    username = request.session.get('user','')
-    search_realname = request.GET.get("name","")
+    username = request.session.get('user', '')
+    search_realname = request.GET.get("name", "")
     guest_list = Guest.objects.filter(realname__contains=search_realname)
     paginator = Paginator(guest_list, 3)
     page = request.GET.get('page')
@@ -68,37 +74,39 @@ def search_realname(request):
         contacts = paginator.page(1)
     except EmptyPage:
         contacts = paginator.page(paginator.num_pages)
-    return render(request,"guest_manage.html",{"user": username, "guests": contacts})
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
 
-#签到页面
+
+# 签到页面
 @login_required
-def sign_index(request,eid):
+def sign_index(request, eid):
     username = request.session.get('user', '')
-    event = get_object_or_404(Event,id=eid)
-    return render(request,'sign_index.html',{'event':event})
+    event = get_object_or_404(Event, id=eid)
+    return render(request, 'sign_index.html', {'event': event})
+
 
 @login_required
-def sign_index_action(request,eid):
+def sign_index_action(request, eid):
     username = request.session.get('user', '')
-    event = get_object_or_404(Event,id=eid)
-    phone = request.POST.get('phone','')
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone', '')
     print(phone)
     result = Guest.objects.filter(phone=phone)
     if not result:
-        return render(request,'sign_index.html',{'event':event,'hint':'phone error !'})
-    result = Guest.objects.filter(phone=phone,event_id=eid)
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'phone error !'})
+    result = Guest.objects.filter(phone=phone, event_id=eid)
     if not result:
-        return render(request,'sign_index.html',{'event':event,'hint':'event id or phone error !'})
-    result = Guest.objects.get(phone=phone,event_id=eid)
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'event id or phone error !'})
+    result = Guest.objects.get(phone=phone, event_id=eid)
     if result.sign:
-        return render(request,'sign_index.html',{'event':event,'hint':'user has sign in !'})
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'user has sign in !'})
     else:
-        Guest.objects.filter(phone=phone,event_id=eid).update(sign='1')
-        return render(request,'sign_index.html',{'event':event,'hint':'sign in success !','guest':result})
+        Guest.objects.filter(phone=phone, event_id=eid).update(sign='1')
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'sign in success !', 'guest': result})
+
 
 @login_required
 def logout(request):
     auth.logout(request)
     response = HttpResponseRedirect('/index/')
     return response
-
